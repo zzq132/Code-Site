60efa40910a1263f872097d141b8c580c0279feb
Zzz <winds2004318@163.com>
2025-05-22 20:55:31 +0800

    initial framework

---
+++ src/main/java/com/zzz/codesite/CodeSiteApplication.java
@@ -0,0 +1,13 @@
+package com.zzz.codesite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSiteApplication.class, args);
+    }
+
+}
